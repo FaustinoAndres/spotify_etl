2dc7b583ff7ffe05020f0927a56ddc21a60fa1f3
Faustino Correa <faustino.correa.m@gmail.com>
2022-05-21 21:52:59 -0400

    request spotify api

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 from decouple import config
+import datetime
+import requests
+import json
 
-print(config('TOKEN'))
+TOKEN = config('TOKEN')
+
+
+if __name__ == '__main__':
+
+    headers = {
+        "Accept" : "application/json",
+        "Content-Type" : "application/json",
+        "Authorization" : "Bearer {token}".format(token=TOKEN)
+    }
+
+
+    # Convert time to Unix timestamp in miliseconds
+    today = datetime.datetime.now()
+    yesterday = today - datetime.timedelta(days=1)
+    yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
+
+    # Download all songs you've listened to "after yesterday", which means in the last 24 hours
+    r = requests.get("https://api.spotify.com/v1/me/player/recently-played?after={time}".format(time=yesterday_unix_timestamp), headers = headers)
+
+    data = r.json()
+
+    print(data)
